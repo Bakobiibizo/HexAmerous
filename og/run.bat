213790f58931fb0d96f808345d9c8f4e4ee5af27
Sebastian Sosa <1sebastian1sosa1@gmail.com>
2024-05-04 17:16:45 -0400

    realocate deprecated hexamorous

---
+++ og/run.bat
@@ -0,0 +1,9 @@
+REM activating env
+call "%CD%\.venv\Scripts\activate.bat"
+echo done
+
+
+REM Run your Python script HexAmerous.py
+echo Running your Python script HexAmerous.py
+call python HexAmerous.py
+echo done
--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Activate the virtual environment
-echo "Activating the virtual environment"
-source .venv/bin/activate
-echo "Done."
-
-# Run your Python script HexAmerous.py
-echo "Running your Python script HexAmerous.py"
-python HexAmerous.py
